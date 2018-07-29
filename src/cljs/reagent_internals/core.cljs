a403035d2651c5e68557e2ea57dc8de907647bcd
Robin Nagpal <rnagpal@rockportnetworks.com>
2018-07-29 18:36:10 -0400

    add different type of components

--- src/cljs/reagent_internals/core.cljs
+++ src/cljs/reagent_internals/core.cljs
@@ -1,7 +1,8 @@
 (ns reagent-internals.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [reagent.ratom :as ratom]
+            [secretary.core :as secretary :include-macros true]
+            [accountant.core :as accountant]))
 
 ;; -------------------------
 ;; Views
@@ -22,17 +23,43 @@
 (defn current-page []
   [:div [@page]])
 
+(def app-state (reagent.ratom/atom {:state-var-1 {:var-a 2
+                                                  :var-b 3}
+                                    :state-var-2 {:var-a 7
+                                                  :var-b 9}}))
+
+(def app-var2-reaction (reagent.ratom/make-reaction
+                         #(get-in @app-state [:state-var-2 :var-a])))
+
+(defn component-using-make-reaction []
+  [:div#component-using-make-reaction
+   [:div>h3 "component-using-make-reaction"]
+   [:div "Sate 2 - var a : " @app-var2-reaction]])
+
+(defn component-using-atom []
+  [:div#component-using-atom
+   [:div>h3 "component-using-atom"]
+   [:div "Sate 1 - var1 : " (get-in @app-state [:state-var-1 :var-a])]])
+
+
+(defn components []
+  [:div
+   [component-using-atom]
+   [component-using-make-reaction]])
+
+
+
 (secretary/defroute "/" []
-  (reset! page #'home-page))
+                    (reset! page #'home-page))
 
 (secretary/defroute "/about" []
-  (reset! page #'about-page))
+                    (reset! page #'about-page))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
+  (reagent/render [components] (.getElementById js/document "app")))
 
 (defn init! []
   (accountant/configure-navigation!
